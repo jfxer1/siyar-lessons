b2bd1fdcbafdc52b1299066513832fdabbadfeb8
jfxer <jfxer@yandex.ru>
2021-07-29 19:46:24 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 84;
+        int b = 34;
+
+        if (!(a > b && b == 34)) {
+            System.out.println("...");
+        } else if (!(a > b && b == 35)){
+            System.out.println("....");
+        } else if(a < b && b ==3) {
+            System.out.println("");
+        } else{
+            System.out.println("");
+        }
+    }
+}
